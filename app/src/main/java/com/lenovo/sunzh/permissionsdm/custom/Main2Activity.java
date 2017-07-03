33985ce9300d798215bc5995947f5fbaa79887a6
sunzhangfei <928256819@qq.com>
2017-07-03 18:45:57 +0800

    软键盘

--- app/src/main/java/com/lenovo/sunzh/permissionsdm/custom/Main2Activity.java
+++ app/src/main/java/com/lenovo/sunzh/permissionsdm/custom/Main2Activity.java
@@ -4,14 +4,16 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import com.lenovo.sunzh.permissionsdm.MainActivity;
 import com.lenovo.sunzh.permissionsdm.R;
 
-public class Main2Activity extends Activity {
+public class Main2Activity extends Activity implements View.OnClickListener {
 
-    private Handler mHandler = new Handler(){
+    private Handler mHandler = new Handler() {
         @Override
         public void handleMessage(Message msg) {
             super.handleMessage(msg);
@@ -19,12 +21,15 @@ public class Main2Activity extends Activity {
         }
     };
     private Button button;
+    private EditText et;
+    private int imeOption = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-        MainActivity.MIUISetStatusBarLightMode(getWindow(),true);
+        MainActivity.MIUISetStatusBarLightMode(getWindow(), true);
         doSth();
     }
 
@@ -32,7 +37,18 @@ public class Main2Activity extends Activity {
         Message obtain = Message.obtain();
         obtain.what = 1;
         mHandler.sendMessage(obtain);
+        et = (EditText) findViewById(R.id.et);
         button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(this);
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button:
+                imeOption++;
+                et.setImeOptions(imeOption % 8);
+                break;
+        }
+    }
 }
